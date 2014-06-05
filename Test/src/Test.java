f5926658c444587a6ecf880b45524c9886430724
amitmallmailbox <amitmallmailbox@gmail.com>
2014-06-05 21:53:02 +0530

    1

---
+++ Test/src/Test.java
@@ -0,0 +1,20 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BinaryCode a = new BinaryCode();
+		
+		System.out.println(a.decode(""));
+	}
+
+}
+
+class BinaryCode {
+	public String[] decode(String message) {
+		for (char a : message.toCharArray()) {
+			
+		}
+		return new String[2];
+	}
+}
